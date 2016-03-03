38ff954cd351fe58ef089e3150d24225b7a40f65
Pulak <pulak.bhattacharyya@symphonyteleca.com>
2016-03-03 13:46:35 +0530

    Angular 2.0 version

--- client/config.js
+++ client/config.js
@@ -1,1 +1,37 @@
-/* File paths */
+module.exports = {
+    dist_dir: 'dist',
+    debug_dir: 'debug',
+    livereload_port: 35729,
+    app_files: {
+        css: [ 'src/styles/**/*.css' ],
+        js: [ 'src/**/*.js', '!src/**/*.spec.js', '!src/assets/**/*.js' ],
+        jsunit: [ 'src/**/*.spec.js' ],
+        atpl: [ 'src/app/**/*.tpl.html' ],
+        ctpl: [ 'src/common/**/*.tpl.html' ],
+        html: [ 'src/index.html' ]
+    },
+    test_files: {
+        js: [
+        ]
+    },
+    vendor_files: {
+        js: [
+            'vendor/bower_components/jquery/dist/jquery.min.js',
+            'vendor/bower_components/angular/angular.js',
+            'vendor/bower_components/angular-resource/angular-resource.min.js',
+            'vendor/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+            'vendor/bower_components/ui-router-extras/release/ct-ui-router-extras.min.js',
+            'vendor/bower_components/slick-carousel/slick/slick.js'
+        ],
+        css: [
+            'vendor/bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'vendor/bower_components/slick-carousel/slick/slick.css',
+            'vendor/bower_components/slick-carousel/slick/slick-theme.css'
+        ],
+        assets: [
+        ]
+    },
+    assets: [
+        'src/assets/**/*'
+    ]
+};
